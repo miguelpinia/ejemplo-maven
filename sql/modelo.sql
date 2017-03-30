761ad876fbaae88da02110f1b50374ff365f0371
Miguel Angel Piña Avelino <miguel_pinia@ciencias.unam.mx>
2017-03-30 08:20:08 -0600

    Agrega el modelo de la base de datos.

---
+++ sql/modelo.sql
@@ -0,0 +1,20 @@
+use miPrimerBase;
+
+drop table if exists persona;
+
+create table if not exists persona (
+  id serial primary key,
+  nombre varchar(255) not null,
+  apellido1 varchar(255) not null,
+  apellido2 varchar(255) not null
+) engine=InnoDB default charset=utf8;
+
+drop table if exists usuario;
+
+create table if not exists usuario (
+  id serial primary key,
+  usuario varchar(255) not null,
+  contraseña varchar(255) not null,
+  persona_id bigint(20) unsigned not null,
+  foreign key(persona_id) references persona(id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
